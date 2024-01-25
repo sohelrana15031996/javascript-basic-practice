84abb4c21963c70ac5f9a5dd44b7989bac1a5645
Sohel Rana <sohelrana130523@gmail.com>
2024-01-25 23:28:04 +0000

    Task-1 done

---
+++ Task-1.js
@@ -0,0 +1,18 @@
+// Task-1
+// You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+// Input:
+// The first line of the input is the taka you have. The second line is the cost of 1 kg of oranges and 1 kg of apples.
+
+// Output:
+// Print the result.
+
+let money = 1500;
+let appleOrangePrice = 1350;
+
+
+let moneyLeft = money - appleOrangePrice;
+
+console.log("You have left " + moneyLeft);
+
+
---
+++ task2.js
@@ -0,0 +1,16 @@
+// Task-2
+// Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+
+// Input:
+// The first line of the input is the marks of the five subjects mentioned above, respectively.
+
+// Output:
+// Print the result in 2 decimal places.
+
+const Mathematics = 100;
+const Biology = 80;
+const Chemistry = 90;
+const Physics = 70;
+const Bangla = 78;
+
+
