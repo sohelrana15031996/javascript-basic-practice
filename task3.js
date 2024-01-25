8b9523306337d74593c5103efc24a25660d7e951
Sohel Rana <sohelrana130523@gmail.com>
2024-01-25 23:35:59 +0000

    Task-3 done

---
+++ task3.js
@@ -0,0 +1,14 @@
+// Task-3
+// You task is to divide the given number by 5 and show the remainder as the output.
+
+// Input:
+// The first line of the input contains the number.
+
+// Output:
+// Print the remainder.
+
+const number =167;
+
+const remainder = 167%5;
+
+console.log("Remainder " + remainder);
