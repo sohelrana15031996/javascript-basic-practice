696d1b0d5129e3c6832a9f7982e9a28dd1e79ad8
Sohel Rana <sohelrana130523@gmail.com>
2024-01-28 21:14:10 +0000

    Declare an array

---
+++ myarry.js
@@ -0,0 +1,11 @@
+const myFruits = ["apple", "orange", "pineaple", "peach", "grape"];
+
+console.log(myFruits[2]);
+myFruits[1] = "jambura"
+console.log(myFruits);
+
+
+
+
+// console.log(myFruits.length);
+// console.log(myFruits);
