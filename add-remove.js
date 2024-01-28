1c4f52ec474a4957fd1d02b8130285b97466fa2f
Sohel Rana <sohelrana130523@gmail.com>
2024-01-28 22:13:34 +0000

    another practice done

---
+++ add-remove.js
@@ -0,0 +1,10 @@
+const myTDestination = ["sajek", "cox's bazar", "Switchzer land"];
+
+myTDestination.push("sunderban");
+console.log(myTDestination);
+
+myTDestination.push("Saint martin", "Darjiling");
+console.log(myTDestination);
+
+myTDestination.pop();
+console.log(myTDestination);
