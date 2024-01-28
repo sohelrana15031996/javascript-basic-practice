846f391bff5433c45f6b6ed5c6eb6c6b14048fe5
Sohel Rana <sohelrana130523@gmail.com>
2024-01-28 22:38:25 +0000

    checking array practice done

---
+++ checkingArry.js
@@ -0,0 +1,9 @@
+const myBooks = "Amar Bangla";
+const programmingLang = ["java", "python", "swift", "javascript"];
+const nameS = ["shujvo", "sweety"];
+const number = 7;
+
+console.log(Array.isArray(myBooks));
+console.log(Array.isArray(programmingLang));
+console.log(Array.isArray(number));
+console.log(Array.isArray(nameS));
