a9091295a4f49dbd3e5f2d57050f3fbbe3b6c467
Sohel Rana <sohelrana130523@gmail.com>
2024-01-28 22:50:43 +0000

    combine arrays

---
+++ combinigArray.js
@@ -0,0 +1,10 @@
+const books = ["bangla", "English", "Math"];
+const moreBooks = ["physics", "Chemistry"];
+const moremoreBooks = ["religion", "sports"]
+console.log(books);
+
+const path = ["Teacher", "Scientist", "Author"];
+console.log(path);
+
+const combined = books.concat(path, moreBooks, moremoreBooks);
+console.log(combined);
